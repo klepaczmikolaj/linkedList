87aa25013abf060518ef35757eb9218e4a8a8ff7
klepaczmikolaj <37023987+klepaczmikolaj@users.noreply.github.com>
2018-03-15 09:56:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,39 +1,64 @@
+/*
+* Klasa realizująca dwukierunkową listę przechowującą liczby całkowite.
+* Umożliwia wykonywanie szereg działań na obiekcie, np. dodawanie, usuwanie elementów, dodawanie, odejmowanie list
+* oraz dostarcza operatory dodawania, porównywania czy kopiowania obiektów.
+*
+* @author Mikołaj Klepacz
+*
+*/
+
 #include <iostream>
 #include "DoublyLinkedList.h"
 
+bool testAdd() {
+    DoublyLinkedList A;
+    A.addNodeStart(5);
+    A.addNodeStart(1);
 
+    return A.getFirstElementByValue(5) && A.getFirstElementByValue(1);
+}
+//Todo: test: all ADD, all DELETE, operator == + =(najlepiej wszystkie), copy constructor
 
 int main()
 {
-    DoublyLinkedList myList;
-    myList.addNodeStart(5);
-    myList.addNodeStart(5);
-    myList.addNodeStart(5);
-    myList.addNodeStart(1);
-    myList.addNodeStart(1);
-    myList.addNodeEnd(1);
+
+    if (!testAdd()) std::cout << "Error in testAdd";
+    DoublyLinkedList A;
+    A.addNodeStart(5);
+    A.addNodeStart(5);
+    A.addNodeStart(5);
+    A.addNodeStart(1);
+    A.addNodeStart(1);
+    A.addNodeEnd(1);
+    A.addNodeEnd(6);
+
+    NodePointer D = A.getFirstElementByValue(5);
+
+    A.printList();
+    cout << D->elementIndex << endl;
+
+    DoublyLinkedList B;
+    B.addNodeStart(10);
+    B.addNodeStart(12);
+    B.addNodeStart(10);
+
+    B.printList();
 
 
-    myList.printList();
-
-    DoublyLinkedList secondList;
-    secondList.addNodeStart(10);
-    secondList.addNodeStart(12);
-    secondList.addNodeStart(10);
-
-    secondList.printList();
+    DoublyLinkedList C;
 
 
-    DoublyLinkedList sumList;
+    C = B;
 
+    C.addNodeStart(10);
+    B.addNodeStart(25);
 
-    sumList = (myList + secondList);
+    B.printList();
+    C.printList();
 
-    sumList.printList();
-    cout << "el numbers: " << sumList.getNumberOfElements() << endl;
+    DoublyLinkedList lista;
+    lista = C;
 
-    sumList = (sumList - secondList);
-    sumList.printList();
 
 
 
